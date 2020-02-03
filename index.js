9c388f5d69b3684d54adfb67d9dd3e56e561952d
kotouno yuta <bananakotono@gmail.com>
2020-02-03 20:21:24 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+window.addEventListener("DOMContentLoaded", init)
+
+function init() {
+  const width = 960
+  const height = 540
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#myCanvas")
+  })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(width, height)
+
+  const scene = new THREE.Scene()
+
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    width / height,
+    1,
+    10000
+  )
+  camera.position.set(0, 0, +2000)
+
+  const geometry = new THREE.BoxGeometry(500, 500, 500)
+  const material = new THREE.MeshStandardMaterial({
+    color: 0x0000ff
+  })
+  const box = new THREE.Mesh(geometry, material)
+  scene.add(box)
+
+  const light = new THREE.DirectionalLight(0xffffff)
+  light.intensity = 2
+  light.position.set(1, 1, 1)
+  scene.add(light)
+
+  tick();
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
+}
